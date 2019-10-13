b04320b9ce4d1009901d00b65972118b11cae99d
Shivam Pandey <shivam.bhumca14@gmail.com>
2019-10-14 01:58:40 +0530

    api server

--- apiServer.js
+++ apiServer.js
@@ -10,12 +10,51 @@ const router = express.Router();
 
 router.get('/', function(request, response){
     console.log("I have been hit...");
+
     response.json({"message": "API is working, let's start working further"});
 });
 
-// router.get("/productDetails", function(request, response) {
-//     response.json();
-// });
+router.get("/productDetails", function(request, response) {
+    console.log("Serving request...");
+    
+    const productDetails = {
+        "name": "Test name",
+        "author": "Test Author",
+        "icon": "https://iconurl",
+        "category": "Transportation",
+        "price": 0,
+        "priceCurrency": "",
+        "inAppBilling": false,
+        "rating": {
+          "count": 21,
+          "display": "4.7",
+          "value": 4.714285850524902,
+          "five": 18,
+          "four": 2,
+          "three": 0,
+          "two": 0,
+          "one": 1
+        },
+        "topDeveloper": false,
+        "screenshots": [
+         "http://a.com",
+         "http://b.com"
+        ],
+        "description": "test long description",
+        "shortDescription": "test short description",
+        "changelog": "Updated user interface colors",
+        "datePublished": "October 11, 2019",
+        "datePublishedIso": "2019-10-011T00:00:00+00:00",
+        "fileSize": "2.9M",
+        "numDownloads": "100 - 500",
+        "versionName": "1.1.0",
+        "operatingSystems": "2.3.3 and up",
+        "storeCategory": "TRAVEL",
+        "contentRating": "Everyone"
+      
+    };
+    response.json(productDetails);
+});
 
 app.use('/api', router);
 
--- index.js
+++ index.js
@@ -1,48 +1,17 @@
-var request = require("request");
 const { GraphQLServer } = require('graphql-yoga')
+const request = require('request');
 
 const resolvers = {
   Query: {
-    productDetails:  () => {
-      return {
-          "name": "Test name",
-          "author": "Test Author",
-          "icon": "https://iconurl",
-          "category": "Transportation",
-          "price": 0,
-          "priceCurrency": "",
-          "inAppBilling": false,
-          "rating": {
-            "count": 21,
-            "display": "4.7",
-            "value": 4.714285850524902,
-            "five": 18,
-            "four": 2,
-            "three": 0,
-            "two": 0,
-            "one": 1
-          },
-          "topDeveloper": false,
-          "screenshots": [
-           "http://a.com",
-           "http://b.com"
-          ],
-          "description": "test long description",
-          "shortDescription": "test short description",
-          "changelog": "Updated user interface colors",
-          "datePublished": "October 11, 2019",
-          "datePublishedIso": "2019-10-011T00:00:00+00:00",
-          "fileSize": "2.9M",
-          "numDownloads": "100 - 500",
-          "versionName": "1.1.0",
-          "operatingSystems": "2.3.3 and up",
-          "storeCategory": "TRAVEL",
-          "contentRating": "Everyone"
-        
-      }
-    },
+    productDetails: () => {
+        request('http://localhost:8081/api/productDetails', { json: true }, (err, res, body) => {
+          if (err) { return console.log(err); }
+          console.log(body.url);
+          console.log(body.explanation);
+        });
+	  }
   },
-}
+};
 
 const options = {
   port: 8000,
